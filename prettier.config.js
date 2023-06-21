de67cfc50c22d92d4e8d63b92ac6693ebba91fa3
Paweł Kieryk <pawelkieryk@gmail.com>
2023-06-21 13:46:03 +0200

    chore: add prettier; config eslint and prettier; update setup

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: "typescript",
+    semi: false,
+    singleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+    arrowParens: "avoid",
+    trailingComma: "es5",
+}
